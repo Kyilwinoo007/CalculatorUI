c270d2e5f6fe125e608959a51dfd0d3be98f59bb
KyiLwinOo <khant.bmruby@gmail.com>
2022-12-15 15:29:27 +0630

    Initial Commit

---
+++ CalculatorUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorUI
+//
+//  Created by Kyi Lwin Oo on 15/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
